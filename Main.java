338f2cb9819477e008e68489230847da3087682c
joshuacassidy <Joshcassidy79@gmail.com>
2018-04-03 21:17:26 +0100

    Reduced else if statement

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         PalindromeChecker palindromeChecker = new PalindromeChecker();
-        System.out.println(palindromeChecker.check("saas"));
+        System.out.println(palindromeChecker.check("ewwwe"));
 
     }
 
--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -1,12 +1,10 @@
 public class PalindromeChecker {
 
     public boolean check(String input) {
-        if (input.length() == 0 || input.length() == 1) {
-            return true;
-        } else if (input.charAt(0) == input.charAt(input.length()-1)){
-            return check(input.substring(1, input.length()-1));
+        if (input.length() == 0 || input.length() == 1 || !(input.charAt(0) == input.charAt(input.length()-1))) {
+            return input.length() == 0 || input.length() == 1;
         } else {
-            return false;
+            return check(input.substring(1, input.length()-1));
         }
 
     }
